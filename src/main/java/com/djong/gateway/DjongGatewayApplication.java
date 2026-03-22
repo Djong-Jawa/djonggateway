46c1bf427e7676c0c0757a2847bfcf99607887d8
RIZKI SADEWA <rizki.sadewa@pegadaian.co.id>
2026-03-22 16:54:25 +0700

    adding some conifg

---
+++ src/main/java/com/djong/gateway/DjongGatewayApplication.java
@@ -0,0 +1,14 @@
+package com.djong.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DjongGatewayApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(DjongGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/djong/gateway/DkivangatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.djong.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DkivangatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
